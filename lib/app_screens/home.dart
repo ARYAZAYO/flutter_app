1dbebde5b1428796e73f5e433743ad70382d5dcc
ARYAZAYO KEITH <aryazayokeith@gmail.com>
2021-12-06 18:46:05 +0300

    Material widget

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -10,17 +10,62 @@ class Home extends StatelessWidget{
       child: Container(
           alignment: Alignment.center,
           color: Colors.deepPurple,
-          child: Text(
-            "Flight",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 75.0,
-              fontFamily: 'Raleway',
-              fontWeight: FontWeight.w700,
-              color: Colors.lightGreen
-            ),
-          ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(child: Text(
+                      "spice Jet",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 35.0,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w700,
+                          color: Colors.lightGreen
+                      )),
+                  ),
+                  Expanded(child:Text(
+                    "From Uganda to Kenya",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 25.0,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w700,
+                        color: Colors.lightGreen
+                    ),
+                  )),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(child: Text(
+                      "Air Tz",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 35.0,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w700,
+                          color: Colors.lightGreen
+                      )),
+                  ),
+                  Expanded(child:Text(
+                    "From Libya to Rwanda",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 25.0,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w700,
+                        color: Colors.lightGreen
+                    ),
+                  )),
+                ],
+              )
+            ],
+          )
       ),
     );
   }
